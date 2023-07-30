2007f6e9ab296cca342980ac8064d4657a38046a
DionysisPatsiouras <dion.patsiouras@gmail.com>
2023-07-30 14:36:29 +0300

    many things improved and build

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,11 +1,10 @@
-import { Outlet, Navigate, Route } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 import { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
 
 
 const PrivateRoutes = ({ children, ...rest }) => {
-    // console.log('Private route works!')
-
+   
     let { user } = useContext(AuthContext)
 
     return (
