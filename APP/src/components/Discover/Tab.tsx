99ffea7d379012fcae01645fb88b11a501c6185b
DionysisPatsiouras <dion.patsiouras@gmail.com>
2024-04-11 15:34:11 +0300

    onClick fix

--- APP/src/components/Discover/Tab.tsx
+++ APP/src/components/Discover/Tab.tsx
@@ -5,9 +5,11 @@ export default function Tab(props: any) {
         <div
             className={CSS.profile_tab}
             onMouseEnter={props?.onMouseEnter}
-            onMouseLeave={props?.onMouseLeave}>
-
-            <li style={{'color' : props?.color}} onClick={props?.onClick}>{props?.label}</li>
+            onMouseLeave={props?.onMouseLeave}
+            onClick={props?.onClick}
+            >
+            
+            <li style={{'color' : props?.color}}>{props?.label}</li>
 
             <span style={{
                 'background': props?.onHover === props?.label || props?.activeTab === props?.label ? props?.color : '#ffffff'
