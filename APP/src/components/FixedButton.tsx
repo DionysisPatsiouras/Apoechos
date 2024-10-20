e14208452b4835d6255a79fba2ba0246b9967047
DionysisPatsiouras <dion.patsiouras@gmail.com>
2024-10-20 21:19:27 +0300

    Update FixedButton.tsx

--- APP/src/components/FixedButton.tsx
+++ APP/src/components/FixedButton.tsx
@@ -8,12 +8,11 @@ interface FixedButtonProps {
     onClick: () => void
 }
 
-// const FixedButton = forwardRef(function FixedButton(props: any, ref: any) {
 const FixedButton = forwardRef(function FixedButton({ icon, onClick }: FixedButtonProps, ref) {
     return (
 
         <div className='circle cursor-pointer' onClick={onClick}>
-            <SvgIcon id={icon} color='#fff' style={{ transform: 'scale(1.5)' }} />
+            <SvgIcon id={icon} color='#fff' style={{ transform: 'scale(1.3)' }} />
         </div>
     )
 })
